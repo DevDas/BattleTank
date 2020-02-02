798ad7c394299203b10631dd55b3fc44491e71b2
DevDas <55701713+DevDas@users.noreply.github.com>
2020-02-03 00:05:57 +0530

    Made The Mortar

--- BattleTank 4.24/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank 4.24/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -100,11 +100,11 @@ void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
 	
 	Barrel->Elevate(DeltaRotator.Pitch); // TODO remove magic
 	// always Yaw The Shortest Way
-	if (DeltaRotator.Yaw < 180)
+	if (FMath::Abs(DeltaRotator.Yaw) < 180)
 	{
 		Turret->Rotate(DeltaRotator.Yaw);
 	}
-	else
+	else // avoid going the long-way round
 	{
 		Turret->Rotate(-DeltaRotator.Yaw);
 	}
