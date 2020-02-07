9de54a9ff54ba94b7fcac6655e697165f4386465
DevDas <55701713+DevDas@users.noreply.github.com>
2020-02-07 22:07:24 +0530

    Tank Suspension Working in Progress

--- BattleTank 4.24/Source/BattleTank/Private/Tank.cpp
+++ BattleTank 4.24/Source/BattleTank/Private/Tank.cpp
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "Tank.h"
 #include "Projectile.h"
 #include "TankBarrel.h"
@@ -14,24 +12,22 @@ float ATank::GetHealthPercent() const
 	return (float)CurrentHealth / (float)StartingHealth;
 }
 
-// Sets default values
 ATank::ATank()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 }
 
 void ATank::BeginPlay()
 {
 	Super::BeginPlay();
-
 	CurrentHealth = StartingHealth;
 }
 
 float ATank::TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser)
 {
 	// Damage Amount is coming from Projectile.h
-	int32 DamagePoints = FPlatformMath::RoundToInt(DamageAmount);  // changing float to int32
+	// changing float to int32
+	int32 DamagePoints = FPlatformMath::RoundToInt(DamageAmount);  
 	int32 DamageToApply = FMath::Clamp(DamagePoints, 0, CurrentHealth);
 
 	CurrentHealth -= DamageToApply;
